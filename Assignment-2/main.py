9d6f5582d5c603f7da6546b46709e93bbd257455
IT23043 <139215343+IT23043@users.noreply.github.com>
2025-05-17 11:12:38 +0600

    Create main.py

---
+++ Assignment-2/main.py
@@ -0,0 +1,21 @@
+import Calc as c
+
+a = int(input("Enter a: "))
+b = int(input("Enter b: "))
+
+while True:
+    choice = int(input("Enter 1 to add, 2 to subtract, 3 to multiply, 4 to divide, or -1 to exit: "))
+
+    if choice == -1:
+        print("Exiting program.")
+        break
+    elif choice == 1:
+        print(a, "+", b, "=", c.add(a, b))
+    elif choice == 2:
+        print(a, "-", b, "=", c.sub(a, b))
+    elif choice == 3:
+        print(a, "*", b, "=", c.multiply(a, b))
+    elif choice == 4:
+        print(a, "/", b, "=", c.divide(a, b))
+    else:
+        print("Invalid choice. Please try again.")
