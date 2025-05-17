346415e4d544190eb712ca2ac04719d018c3498e
IT23043 <139215343+IT23043@users.noreply.github.com>
2025-05-17 11:12:06 +0600

    Create calc.py

---
+++ Assignment-2/calc.py
@@ -0,0 +1,17 @@
+def add(a, b):
+    return a + b
+
+def sub(a, b):
+    if a >= b:
+        return a - b
+    else:
+        return b - a
+
+def multiply(a, b):
+    return a * b
+
+def divide(a, b):
+    if b != 0:
+        return a / b
+    else:
+        return -1
